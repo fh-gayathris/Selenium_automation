5e1700185123df9c664219f1a482450489007832
fh-gayathris <gayathri.saravanan@foodhub.com>
2025-12-29 17:46:26 +0530

    Updated boostrapmodals

--- seleniumtask2.java
+++ seleniumtask2.java
@@ -15,30 +15,25 @@ public class seleniumtask2 {
         driver.manage().window().maximize();
         driver.get("https://www.lambdatest.com/selenium-playground/");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        //dynamic data loading
-        System.out.println("Dynamic data Loading");
 
+
+        System.out.println("Dynamic data Loading");
         WebElement dataLoad= driver.findElement(By.xpath("//a[contains(text(),'Dynamic Data Loading')]"));
         dataLoad.click();
         WebElement getUser=driver.findElement(By.id("save"));
         getUser.click();
         WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(10));
-
         WebElement name= driver.findElement(By.id("loading"));
-
         name.click();
-
         wait.until(ExpectedConditions.textToBePresentInElementLocated(
                 By.id("loading"), "First Name"
         ));
-
-
         System.out.println(name.getText());
         driver.navigate().back();
         System.out.println();
 
-        //context menu
-        System.out.println("Context Menu");
+
+        System.out.println("Context Menu(alert handles)");
         WebElement contextMenu=driver.findElement(By.xpath("//a[contains(text(),'Context Menu')]"));
         contextMenu.click();
         WebElement divBox= driver.findElement(By.id("hot-spot"));
@@ -51,7 +46,7 @@ public class seleniumtask2 {
         driver.navigate().back();
         System.out.println();
 
-        //table paginaiton
+
 
         System.out.println("Table pagination");
         WebElement tablePagination=driver.findElement(By.xpath("//a[contains(text(),'Table Pagination')]"));
@@ -62,26 +57,15 @@ public class seleniumtask2 {
         option.click();
         WebElement pageNo=driver.findElement(By.xpath("//span[contains(text(),\"3\")]"));
         pageNo.click();
-        List<WebElement> rows =
-                driver.findElements(By.xpath("//table/tbody/tr[not(contains(@style,'display: none'))]"));
-
-        String email = rows.get(6)
-                .findElements(By.tagName("td"))
-                .get(3)
-                .getText();
-
-        String phone = rows.get(6)
-                .findElements(By.tagName("td"))
-                .get(4)
-                .getText();
-
+        List<WebElement> rows = driver.findElements(By.xpath("//table/tbody/tr[not(contains(@style,'display: none'))]"));
+        String email = rows.get(6).findElements(By.tagName("td")).get(3).getText();
+        String phone = rows.get(6).findElements(By.tagName("td")).get(4).getText();
         System.out.println("Email: " + email);
         System.out.println("Phone: " + phone);
-
         driver.navigate().back();
         System.out.println();
 
-        //window pop up modal
+
         System.out.println("Window pop up modal");
         WebElement windowPop=driver.findElement(By.xpath("//a[contains(text(),'Window Popup Modal')]"));
         windowPop.click();
@@ -89,12 +73,10 @@ public class seleniumtask2 {
         followAll.click();
         String parentWindow = driver.getWindowHandle();
         Set<String> allWindows = driver.getWindowHandles();
-
         for (String window : allWindows) {
             driver.switchTo().window(window);
             if (!window.equals(parentWindow)) {
                 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
                 System.out.println("URL: " + driver.getCurrentUrl());
                 System.out.println("Title: " + driver.getTitle());
                 driver.close();
@@ -104,7 +86,7 @@ public class seleniumtask2 {
         driver.navigate().back();
         System.out.println();
 
-        //nested frames
+
 
         System.out.println("Nested frames");
         WebElement nestedFrames=driver.findElement(By.xpath("//a[contains(text(),'Nested Frames')]"));
@@ -115,27 +97,39 @@ public class seleniumtask2 {
         driver.switchTo().frame("frame-bottom");
 
         List<WebElement> frames = driver.findElements(By.tagName("frame"));
-
         for (WebElement frame : frames) {
             driver.switchTo().frame(frame);
             System.out.println(driver.findElement(By.tagName("body")).getText());
-            driver.switchTo().parentFrame(); // go back to frame-bottom
+            driver.switchTo().parentFrame();
         }
         driver.switchTo().defaultContent();
         driver.navigate().back();
-
         System.out.println();
 
-        //Bootstrap modals
-
-
-        WebElement bootsrapModals=driver.findElement(By.xpath("//a[contains(text(),'Bootstrap Modals')]"));
+        WebElement bootsrapModals = driver.findElement(By.xpath("//a[contains(text(),'Bootstrap Modals')]"));
         bootsrapModals.click();
-
-        WebElement launchModal1=driver.findElement(By.xpath("//button[@data-target='#myModal']"));
+ WebElement launchModal1 = driver.findElement(By.xpath("//button[@data-target='#myModal']"));
         launchModal1.click();
-        WebElement saveChanges=driver.findElement(By.xpath("(//*[text()='Save Changes'])[1]"));
+        WebElement saveChanges = wait.until(
+                ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='myModal']//*[text()='Save Changes']")));
         saveChanges.click();
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(By.id("myModal")));
+        WebElement launchModal2 = wait.until(ExpectedConditions.elementToBeClickable(
+                        By.xpath("//button[@data-target='#myMultiModal']")));
+        launchModal2.click();
+        WebElement secondModalBtn = wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//button[@data-target='#mySecondModal']")));
+        secondModalBtn.click();
+        WebElement saveChanges2 = wait.until(ExpectedConditions.elementToBeClickable(
+                        By.xpath("//div[@id='mySecondModal']//*[text()='Save Changes']")));
+        saveChanges2.click();
+
+        driver.navigate().back();
+
+
+
+
+
 
 
 
